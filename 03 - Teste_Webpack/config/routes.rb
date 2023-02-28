d03c253b7e023b85bc1445df253e639a4067ddcb
tiellesa@gmail.com <tiellesa@id.uff.br>
2023-02-28 16:54:03 -0300

    Exercícios Módulo 5 - 117 - 129.

--- 03 - Teste_Webpack/config/routes.rb
+++ 03 - Teste_Webpack/config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  namespace :site do
+    end
   resources :planta
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  get 'index', to: 'welcome#index'
 end
